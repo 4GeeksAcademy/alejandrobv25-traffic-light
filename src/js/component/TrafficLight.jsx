ac7611657e09946c2c62f947d89eb25172f6d272
Alejandrobv25 <alexbavierasaiz@gmail.com>
2024-12-21 15:53:05 +0000

    first commit

---
+++ src/js/component/TrafficLight.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+export const TrafficLight = () => {
+    const [background, setBackground] = useState('rojo');
+
+    return(
+        <div className="card">
+            <div className="luces">
+                <div className={`rojo ${background == "rojo" ? "active" : ""}`} onClick={() => setBackground("rojo")}></div>
+                <div className={`amarillo ${background == "amarillo" ? "active" : ""}`} onClick={() => setBackground("amarillo")}></div>
+                <div className={`verde ${background == "verde" ? "active" : ""}`} onClick={() => setBackground("verde")}></div>
+            </div>
+
+        </div>
+    )
+
+}
+
